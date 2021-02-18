4f9728be65864d4730a28ccb8a697cac637ff0ee
moises.alcocer <contacto@ironwoods.es>
2021-02-19 00:23:14 +0100

    seeded categories

--- database/seeds/CategorySeeder.php
+++ database/seeds/CategorySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class CategorySeeder extends Seeder
@@ -13,6 +14,26 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        factory(Category::class, 20)->create();
+
+        $categories = Category::all();
+        $this->setSubcategories($categories[0], $categories, [2, 3, 4]);
+        $this->setSubcategories($categories[1], $categories, [10, 12, 18]);
+        $this->setSubcategories($categories[2], $categories, [5, 6]);
+    }
+
+
+    private function setSubcategories(
+        Category $parentCategory,
+        object $categories,
+        array $positions
+    ): void
+    {
+        $parentCategoryId = $parentCategory->id;
+        foreach ($positions as $position) {
+            $category = $categories[$position];
+            $category->parent_id = $parentCategoryId;
+            $category->save();
+        }
     }
 }
