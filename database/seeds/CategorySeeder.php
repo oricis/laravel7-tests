310f924719f0796f04a7c6bac27719a33767b838
moises.alcocer <contacto@ironwoods.es>
2021-02-18 23:50:14 +0100

    added model, migration & seeder for "categories"

---
+++ database/seeds/CategorySeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Seeders;
+
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
