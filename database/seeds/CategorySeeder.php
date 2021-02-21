39f51fef0da82e7972be1a7e84b58c2bbec0cbff
moises.alcocer <contacto@ironwoods.es>
2021-02-21 23:49:39 +0100

    added order field to categories

--- database/seeds/CategorySeeder.php
+++ database/seeds/CategorySeeder.php
@@ -20,8 +20,18 @@ class CategorySeeder extends Seeder
         $this->setSubcategories($categories[0], $categories, [2, 3, 4]);
         $this->setSubcategories($categories[1], $categories, [10, 12, 18]);
         $this->setSubcategories($categories[2], $categories, [5, 6]);
+
+        $this->addOrders(Category::whereOrder(0)->get(['id', 'order']));
     }
 
+
+    private function addOrders(object $categories): void
+    {
+        foreach ($categories as $key => $category) {
+            $category->order = $key + 1;
+            $category->save();
+        }
+    }
 
     private function setSubcategories(
         Category $parentCategory,
@@ -30,9 +40,10 @@ class CategorySeeder extends Seeder
     ): void
     {
         $parentCategoryId = $parentCategory->id;
-        foreach ($positions as $position) {
+        foreach ($positions as $key => $position) {
             $category = $categories[$position];
             $category->parent_id = $parentCategoryId;
+            $category->order = $key + 1;
             $category->save();
         }
     }
