a73a3aa11acb4c969d0e2619faecc321a68498cd
moises.alcocer <contacto@ironwoods.es>
2021-03-16 23:46:05 +0100

    added API routes to fetch tests

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -76,5 +76,10 @@ class RouteServiceProvider extends ServiceProvider
             ->middleware('api')
             ->namespace($this->namespace)
             ->group(base_path('routes/api.php'));
+
+        Route::prefix('api')
+            ->middleware('api')
+            ->namespace($this->namespace)
+            ->group(base_path('routes/fetch-test.php'));
     }
 }
